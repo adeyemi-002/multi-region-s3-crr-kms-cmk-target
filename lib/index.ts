65994dd77e034a5f0ddc8d1e07c0863579e0d83e
adeyemi <ade.sobaki@gmail.com>
2023-05-13 20:09:54 +0200

    first cdk project

--- lib/index.ts
+++ lib/index.ts
@@ -1,21 +1,11 @@
-// import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-
-export interface MultiRegionS3CrrKmsCmkTargetProps {
-  // Define construct properties here
-}
+import * as cdk from 'aws-cdk-lib'
+import { Construct } from 'constructs'
+import * as kms from 'aws-cdk-lib/aws-kms'
 
 export class MultiRegionS3CrrKmsCmkTarget extends Construct {
+  constructor(scope: Construct, id: string) {
+    super(scope, id)
 
-  constructor(scope: Construct, id: string, props: MultiRegionS3CrrKmsCmkTargetProps = {}) {
-    super(scope, id);
-
-    // Define construct contents here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'MultiRegionS3CrrKmsCmkTargetQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const targetKmsKey = new kms.Key(this, 'MyTargetKey')
   }
 }
